53f1e4ad5fb73f379c50fcbdcb8f373ae7ad564a
techmoves <muhndlovu28@gmail.com>
2023-06-01 12:32:24 +0200

    Updated the popup

---
+++ src/popup.js
@@ -0,0 +1,47 @@
+const popup = ( ) => {
+    const coment = document.createElement('div');
+    coment.clasname = pop
+}
+
+
+const work = document.getElementById('movie-display');
+
+movie-display.forEach(() => {
+  const div = document.createElement('div');
+  div.id = 'movie-display';
+  if () {
+    div.id = index;
+    div.innerHTML = `
+  <div id="movie-display">
+        
+          <div class="photo1">
+            <span id="closeButton"><i class="fa-solid fa-xmark"></i></span>
+            
+              <img class="movi-pic" src="https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" alt="movie phonto">
+              
+          </div>
+  
+          <h2> The Black list</h2>
+          <div class="deatails">
+              <div class="df1">
+                  <h2>Language:English</h2>
+                  <h2>Estimated time:2hrs</h2>
+              </div>
+              <div class="df2">
+                  <h2>Movie genres:Drama</h2>
+                  <h2>Rating:5.5</h2>
+              </div>
+          </div>
+          <h2>Summary:</h2>
+          <p class="summary">
+              The Blacklist is a crime drama involving a former government agent who turned into a high-profile criminal
+              turning himself in to the FBI offering to help catch criminals.
+          </p>
+          <div class="whole-coment">
+              <h3>Add your comments</h3>
+              
+          </div>>
+    `
+  };
+
+
