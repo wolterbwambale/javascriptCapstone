e719009eb865d8fac2f233426dcdcf78dea3f318
wolterbwambale <benplaan1@gmail.com>
2023-06-02 16:11:23 +0300

    update the comment popup

--- src/retrieveMovies.js
+++ src/retrieveMovies.js
@@ -3,7 +3,9 @@ const retrieveshows = async () => {
 
   const request = await fetch('https://api.tvmaze.com/shows');
   const response = await request.json();
+  console.log(response)
   return response;
+  
 }
 
 const updateLikeCount = async (itemId, updatedCount) => {
@@ -128,91 +130,86 @@ const displayShows = async () => {
 }
 displayShows();
 
-
-const movieDisplay = [
-  {
-    image: 'https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg',
-    title: 'The Blacklist',
-    language: 'English',
-    duration: '2hrs',
-    genres: 'Drama',
-    rating: '5.5',
-    summary: 'The Blacklist is a crime drama involving a former government agent who turned into a high-profile criminal turning himself in to the FBI offering to help catch criminals.'
-  }
-];
-
+/*popup*/
 const container = document.getElementById('work');
 
-movieDisplay.forEach((item, index) => {
-  const movieDiv = document.createElement('div');
-  movieDiv.id = `movie-display-${index}`;
-  movieDiv.className = 'popup';
-  movieDiv.innerHTML = `
-    <div class="popup-content">
-      <span id="closePopupButton-${index}" class="close-popup-button"><i class="fa-solid fa-xmark"></i></span>
-      <div class="photo1">
-        <img class="movie-pic" src="${item.image}" alt="movie photo">
-      </div>
-      <h2>${item.title}</h2>
-      <div class="details">
-        <div class="df1">
-          <h2>Language: ${item.language}</h2>
-          <h2>Estimated time: ${item.duration}</h2>
-        </div>
-        <div class="df2">
-          <h2>Movie genres: ${item.genres}</h2>
-          <h2>Rating: ${item.rating}</h2>
-        </div>
-      </div>
-      <h2 class=comment-count>comments(3)</h2>
-      <span>12/12/2012:alex i like the product</span><br>
-      <span>12/12/2012:alex i like the product</span>
-      <h2>Summary:</h2>
-      <p class="summary">
-        ${item.summary}
-      </p>
-      <div class="whole-comment">
-        <h3>Add your comments</h3>
-      </div>
-      <form id="popupForm-${index}" class="form">
-        <div class="flex-form">
-          <input type="text" name="name" placeholder="Your Name" class="name" id="name-${index}"> <br>
-          <textarea
-            name="text-message"
-            class="type-text message"
-            cols="30"
-            rows="10"
-            placeholder="Your insight"
-            id="message-${index}"
-            maxlength="500"
-            required
-          ></textarea>
-        </div>
-        <button id="comment" type="submit">Comment</button>
-      </form>
-    </div>
-  `;
+const fetchMovieData = async () => {
+  try {
+    const response = await fetch('https://api.tvmaze.com/shows');
+    const data = await response.json();
 
-  container.appendChild(movieDiv);
-});
+    const movies = Object.values(data); // Convert the response object to an array
+
+    movies.forEach((item, index) => {
+      const movieDiv = document.createElement('div');
+      movieDiv.id = `movie-display-${index}`;
+      movieDiv.className = 'popup';
+      movieDiv.innerHTML = `
+        <div class="popup-content">
+          <span id="closePopupButton-${index}" class="close-popup-button"><i class="fa-solid fa-xmark"></i></span>
+          <div class="photo1">
+            <img class="movie-pic" src="${item.image.medium}" alt="movie photo">
+          </div>
+          <h2>${item.name}</h2>
+          <div class="details">
+            <div class="df1">
+              <h2>Language: ${item.language}</h2>
+              <h2>Estimated time: ${item.runtime} min</h2>
+            </div>
+            <div class="df2">
+              <h2>Movie genres: ${item.genres.join(', ')}</h2>
+              <h2>Rating: ${item.rating.average}</h2>
+            </div>
+          </div>
+          <h2 class="comment-count">comments(3)</h2>
+          <span>12/12/2012:alex i like the product</span><br>
+          <span>12/12/2012:alex i like the product</span>
+          <h2>Summary:</h2>
+          <p class="summary">
+            ${item.summary}
+          </p>
+          <div class="whole-comment">
+            <h3>Add your comments</h3>
+          </div>
+          <form id="popupForm-${index}" class="form">
+            <div class="flex-form">
+              <input type="text" name="name" placeholder="Your Name" class="name" id="name-${index}"> <br>
+              <textarea
+                name="text-message"
+                class="type-text message"
+                cols="30"
+                rows="10"
+                placeholder="Your insight"
+                id="message-${index}"
+                maxlength="500"
+                required
+              ></textarea>
+            </div>
+            <button id="comment" type="submit">Comment</button>
+          </form>
+        </div>
+      `;
+
+      container.appendChild(movieDiv);
+
+      const closeButton = document.getElementById(`closePopupButton-${index}`);
+      closeButton.addEventListener('click', () => {
+        closePopup(index);
+      });
+    });
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+  }
+};
+
+fetchMovieData();
 
 const openPopup = (index) => {
   const popup = document.getElementById(`movie-display-${index}`);
   popup.style.display = 'block';
-}
+};
 
 const closePopup = (index) => {
   const popup = document.getElementById(`movie-display-${index}`);
   popup.style.display = 'none';
-}
-
-movieDisplay.forEach((item, index) => {
-  const closeButton = document.getElementById(`closePopupButton-${index}`);
-  closeButton.addEventListener('click', function() {
-    closePopup(index);
-  });
-});
-
-
-// Call the displayShows function
-
+};
